47350d2528ae4ad37215210b57c5f5a6b7fc9ad6
Merajul Islam <aslammeraj12345@gmail.com>
2026-05-09 00:45:28 +0530

    Update upload.js

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -22,6 +22,7 @@ const fileFilter = (req, file, cb) => {
   cb(null, true);
 };
 
+
 const upload = multer({ storage, fileFilter, limits: { fileSize: 5 * 1024 * 1024 } });
 
 module.exports = upload;
